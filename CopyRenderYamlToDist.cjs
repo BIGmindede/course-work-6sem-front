a4411d73a56071d1f4ec8e9a652da45b257b8f45
Nikita Khitrov <hitrov.qazws@gmail.com>
2024-04-12 01:32:15 +0300

    fix order of building cmds

--- CopyRenderYamlToDist.cjs
+++ CopyRenderYamlToDist.cjs
@@ -5,8 +5,8 @@ const sourceFilePath = path.resolve(__dirname, 'render.yaml')
 const destFilePath = path.resolve(__dirname, 'dist/render.yaml')
 
 try {
-  copyFileSync(sourceFilePath, destFilePath)
-  console.log('File render.yaml successfully copied to dist folder')
+    fs.copyFileSync(sourceFilePath, destFilePath)
+    console.log('File render.yaml successfully copied to dist folder')
 } catch (err) {
-  console.error(err)
+    console.error(err)
 }
