092abac6ce7c0ec691f051b5e42bea34c7be360b
Nikita Khitrov <hitrov.qazws@gmail.com>
2024-05-05 14:19:58 +0300

    fix bug month in date 1 less than real

--- src/shared/lib/transformDate.js
+++ src/shared/lib/transformDate.js
@@ -1,7 +1,7 @@
 export const transformDate = (date) => {
     date = new Date(date)
     const y = date.getFullYear()
-    const m = date.getMonth()
+    const m = date.getMonth() + 1
     const d = date.getDate()
     return `${d < 10 ? "0" + d : d}.${m < 10 ? '0' + m : m}.${y}`
 }
