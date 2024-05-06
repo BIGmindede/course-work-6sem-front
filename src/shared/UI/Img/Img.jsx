e70707e281c79275e8e75deff8fd140d9a84876d
Nikita Khitrov <hitrov.qazws@gmail.com>
2024-05-06 19:41:50 +0300

    changed notfound from static picture to svg

--- src/shared/UI/Img/Img.jsx
+++ src/shared/UI/Img/Img.jsx
@@ -1,18 +1,31 @@
 import { useState } from "react"
 import { STATIC_URL } from "shared/config/http"
-
-export const Img = ({ imageId }) => {
+import PictureNotFoundIcon from 'shared/assets/icons/PictureNotFoundIcon.svg?react'
+import cls from './Img.module.scss'
+import { useClassNames } from "shared/lib/useClassNames"
+export const Img = ({ imageId, className }) => {
 
     const [image, setImage] = useState(`${STATIC_URL}/${imageId}`)
+    const [notFound, setNotFound] = useState(false)
+    const classname = useClassNames(cls.img, [className, !notFound && cls.stub])
 
     return (
-        <img
-            src={image}
-            onError={(e) => {
-                e.target.onError = null
-                setImage('pictureNotFound.jpg')
-            }}
-            alt=""
-        />
+        <>
+        {notFound
+            ? <img
+                className={classname}
+                src={image}
+                onError={(e) => {
+                    e.target.onError = null
+                    setImage('pictureNotFound.jpg')
+                    setNotFound(true)
+                }}
+                alt=""
+            />
+            : <div className={classname}>
+                <PictureNotFoundIcon/>
+            </div>
+        }
+        </>
     )
 }
