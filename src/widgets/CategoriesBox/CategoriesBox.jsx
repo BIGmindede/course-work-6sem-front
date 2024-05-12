41423e7853bd32a6f38233dcfb1930c37d96c2c1
Nikita Khitrov <hitrov.qazws@gmail.com>
2024-05-12 19:55:36 +0300

    fix add loader and empty placeholder to categories box

--- src/widgets/CategoriesBox/CategoriesBox.jsx
+++ src/widgets/CategoriesBox/CategoriesBox.jsx
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom'
 import { truncate } from 'shared/lib/truncate'
 import { Img } from 'shared/UI/Img/Img'
 import { getFiltersQuery } from 'shared/lib/getFiltersQuery'
+import { Loader } from 'shared/UI/Loader/Loader'
 
 export const CategoriesBox = ({ className }) => {
 
@@ -21,18 +22,21 @@ export const CategoriesBox = ({ className }) => {
 
     return (
         <div className={useClassNames(cls.categoriesbox, [cls[className]])}>
-            {
+            {!loading 
+                ? categoriesList.length > 0
+                    ? categoriesList.map(category => 
+                        <Link key={category.id} to={`/search?${getFiltersQuery({category: category.title})}`}>
+                            <div className={cls.categorywrapper}>
+                                <div className={cls.imagewrapper}>
+                                    <Img imageId={category.pictureName}/>
+                                </div>
+                                <div className={cls.categorytitle}>{truncate(category.title, 18)}</div>
+                            </div>
+                        </Link>
+                    )
+                    : <div className={cls.emptyplaceholder}>Пусто</div>
+                : <Loader/>
             }
-            {categoriesList.map(category => 
-                <Link key={category.id} to={`/search?${getFiltersQuery({category: category.title})}`}>
-                    <div className={cls.categorywrapper}>
-                        <div className={cls.imagewrapper}>
-                            <Img imageId={category.pictureName}/>
-                        </div>
-                        <div className={cls.categorytitle}>{truncate(category.title, 18)}</div>
-                    </div>
-                </Link>
-            )}
         </div>
     )
 }
